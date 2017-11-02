3137d226a5f9e8e5de9abc85e749aeaebfe13657
onetechwolf <onetechwolf@gmail.com>
2017-11-03 02:33:07 +0400

    removed package-lock.json from gitignore

--- readme.md
+++ readme.md
@@ -18,50 +18,42 @@ Please follow the guide.
 5. `php artisan db:seed`
 6. `npm install`
 
-## Install Passport
-
-Open a terminal window and install the passport using following command
+open a terminal window and install the passport using following command
 
  ```
  php artisan passport:install
  ```
-## Update the Passport keys in .env file 
-Copy the keys for personal and password grants in `.env` file
+ 
+copy the keys for personal and password grants in .env file
 
 ```
 PERSONAL_CLIENT_ID=1
+
 PERSONAL_CLIENT_SECRET=xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
 PASSWORD_CLIENT_ID=2
+
 PASSWORD_CLIENT_SECRET=xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 ```
-## Set the App URL
-Set the APP_URL in `.env` file (e.g)
+
+set the APP_URL in .env file (e.g)
 
 ```
 APP_URL=http://localhost:8000
 ```
 
-## Set the APP Title
-Set the APP_TITLE in `resources/assets/js/values/index.js`
-
-```angular2html
-APP_TITLE='Your Blog Name'
-```
-
-## Run PHP Dev Server
 Either create a local dev url and map the link in webpack.mix.js file or open an other terminal window and copy paste the following command
 
 ```
 php artisan serve
 ```
 
-## Run Node Engine
+then run the following command on terminal to compile the assets
 
-Compile assets one time.
 ```
 npm run dev
 ```
-**OR**
+
 or if you would like to compile assets on runtime then copy paste following command in terminal 
 
 `npm run watch` or `npm run watch-poll`
--- resources/assets/js/pages/articles/show/Page.js
+++ resources/assets/js/pages/articles/show/Page.js
@@ -1,82 +1,76 @@
 // import libs
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import DocumentTitle from 'react-document-title';
-import {articleFetchRequest} from '../../../store/services/article'
-import {APP_TITLE} from '../../../values/index'
+import { articleFetchRequest } from '../../../store/services/article'
 
 class Page extends Component {
-    static displayName = 'ArticleShowPage'
-    static propTypes = {
-        match: PropTypes.object.isRequired,
-        article: PropTypes.object.isRequired,
-        dispatch: PropTypes.func.isRequired
+  static displayName = 'ArticleShowPage'
+  static propTypes = {
+    match: PropTypes.object.isRequired,
+    article: PropTypes.object.isRequired,
+    dispatch: PropTypes.func.isRequired
+  }
+  
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      //
     }
-
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            //
-        }
+  }
+  
+  componentWillMount() {
+    this.loadArticle()
+  }
+  
+  loadArticle() {
+    const { match, article, dispatch } = this.props
+    
+    if (!article.id) {
+      dispatch(articleFetchRequest(match.params.id))
     }
-
-    componentWillMount() {
-        this.loadArticle()
+  }
+  
+  renderPublishedDate() {
+    const { publishedAt } = this.props.article
+    
+    if (publishedAt) {
+      return `at ${publishedAt.format('MMMM d, YYYY')}`
     }
-
-    loadArticle() {
-        const {match, article, dispatch} = this.props
-
-        if (!article.id) {
-            dispatch(articleFetchRequest(match.params.id))
-        }
+  }
+  
+  renderAuthor() {
+    const { user } = this.props.article
+    
+    if (user) {
+      return `by ${user.name}`
     }
-
-    renderPublishedDate() {
-        const {publishedAt} = this.props.article
-
-        if (publishedAt) {
-            return `at ${publishedAt.format('MMMM d, YYYY')}`
-        }
-    }
-
-    renderAuthor() {
-        const {user} = this.props.article
-
-        if (user) {
-            return `by ${user.name}`
-        }
-
-    }
-
-    createMarkup() {
-        return {__html: this.props.article.content};
-    }
-
-    renderArticle() {
-        const {article} = this.props
-        return (<div className="col-12 col-sm-9 mb-5 mx-auto">
-            <h2>{article.title}</h2>
-            <small className="text-muted mb-5">{this.renderPublishedDate()} {this.renderAuthor()}</small>
-            <p className="text-muted mb-5">{article.description}</p>
-            <div dangerouslySetInnerHTML={this.createMarkup()}/>
-        </div>)
-    }
-
-    render() {
-        return (
-            <DocumentTitle title={`${this.props.article.title} - ${APP_TITLE}`}>
-                <section id="components-articles">
-                    <div className="container">
-                        <div className="row">
-                            {this.renderArticle()}
-                        </div>
-                    </div>
-                </section>
-            </DocumentTitle>
-        )
-    }
+    
+  }
+  
+  createMarkup() {
+    return {__html: this.props.article.content};
+  }
+  
+  renderArticle() {
+    const { article } = this.props
+    return (<div className="col-12 col-sm-9 mb-5 mx-auto">
+      <h2>{article.title}</h2>
+      <small className="text-muted mb-5">{ this.renderPublishedDate() } { this.renderAuthor() }</small>
+      <p className="text-muted mb-5">{article.description}</p>
+      <div dangerouslySetInnerHTML={this.createMarkup()} />
+    </div>)
+  }
+  
+  render() {
+    return(<section id="components-articles">
+      <div className="container">
+        <div className="row">
+          {this.renderArticle()}
+        </div>
+      </div>
+    </section>)
+  }
 }
 
 export default Page
